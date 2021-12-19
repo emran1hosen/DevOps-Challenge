dd703f0c0a69f89e229016993e090d41de057610
emran1hosen <96368903+emran1hosen@users.noreply.github.com>
2021-12-19 16:48:58 +0600

    Update env_readiness.sh

--- env_readiness.sh
+++ env_readiness.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 sudo apt-get update
-#install git
 sudo apt-get install git -y
-#Clone repo
 cd /home/ubuntu
 sudo git clone https://github.com/emran1hosen/DevOps-Challenge.git
-#Give user permission to the code directory for executional purpose
 sudo chown -R ubuntu:ubuntu DevOps-Challenge
-#install dependencies i.e curl, php7.4 and composer 
 sudo apt-get update
 sudo apt install curl -y
-sudo apt-get install php php7.4 php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
+sudo apt-get install php -y
+sudo apt-get install php7.4
+sudo apt-get install php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
+cd /home/ubuntu
+export COMPOSER_HOME="$HOME/.config/composer"
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+cd /home/ubuntu/DevOps-Challenge/app/
+composer install
+composer update
+php artisan key:generate
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y \
@@ -28,9 +35,7 @@ sudo apt-cache madison docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
-#checking docker installation 
 sudo docker run hello-world 
-#Preparing to containerized the application in docker container
 sudo usermod -aG docker ubuntu 
 sudo service apache2 stop -y
 cd /home/ubuntu/DevOps-Challenge/

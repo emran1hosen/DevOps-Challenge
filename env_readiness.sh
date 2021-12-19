a55aad62b652fffc306460628daa62100007287d
emran1hosen <96368903+emran1hosen@users.noreply.github.com>
2021-12-19 16:39:12 +0600

    Update env_readiness.sh

--- env_readiness.sh
+++ env_readiness.sh
@@ -10,20 +10,7 @@ sudo chown -R ubuntu:ubuntu DevOps-Challenge
 #install dependencies i.e curl, php7.4 and composer 
 sudo apt-get update
 sudo apt install curl -y
-sudo apt-get install php -y
-sudo apt-get install php7.4
-sudo apt-get install php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
-cd /home/ubuntu
-export COMPOSER_HOME="$HOME/.config/composer"
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
-#install laravel project dependencies and build the project
-cd /home/ubuntu/DevOps-Challenge/app/
-composer install
-composer update
-php artisan key:generate
-#Install docker and dependencies
+sudo apt-get install php php7.4 php7.4-mbstring php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-zip -y
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y \
